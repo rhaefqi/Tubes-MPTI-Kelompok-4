b57537fb88887194b2e86463aa16fffecb13854e
rhaefqi <rifqijabrah@gmail.com>
2024-11-21 20:56:01 +0700

    perbaikan commit 2

--- routes/auth.php
+++ routes/auth.php
@@ -22,8 +22,6 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('userlogin', [AuthenticatedSessionController::class, 'userStore'])
-                ->name('user.login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
